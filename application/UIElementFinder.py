629d4ae25af98ba0337ff0f86ced199cd7262358
Sebastian <epicsauce2013@gmail.com>
2024-11-29 04:41:54 -0800

    added some overload methods for futre implmentation of the UIElementFinder class

--- application/UIElementFinder.py
+++ application/UIElementFinder.py
@@ -25,3 +25,34 @@ class UIElementFinder():
         found_window_objs = self.window.findChild(QObject, widgetNameToFind)
         return found_window_objs
     
+    
+    
+    
+    
+    
+    def find_Objects(window: QMainWindow, widgetsToFind: list[QObject]):
+        found_window_objs = []
+        for obj in widgetsToFind:
+            obj_found = window.findChild(obj)
+            found_window_objs.append(obj_found)
+        
+        return found_window_objs
+
+    def find_anObject(window: QMainWindow, widgetToFind: QObject):
+        found_window_objs = window.findChild(widgetToFind)
+        return found_window_objs
+    
+    
+    
+    
+    def find_Objects(mainWidget: QWidget, widgetsToFind: list[QObject]):
+        found_window_objs = []
+        for obj in widgetsToFind:
+            obj_found = mainWidget.findChild(obj)
+            found_window_objs.append(obj_found)
+        
+    
+    def find_anObject(mainWidget: QWidget, widgetToFind: QObject):
+        # Search for the widget by type or object name within mainWidget
+        obj_found = mainWidget.findChild(widgetToFind)
+        return obj_found
